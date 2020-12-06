a4a5341802affe0bec5a0a0009bc9190057afdd2
RedoC-github <coderlovemedia@gmail.com>
2020-12-06 21:52:46 +0900

    bug: fix error

--- test/proto_basic_test.cpp
+++ test/proto_basic_test.cpp
@@ -11,15 +11,15 @@
 
 int main() {
     // perform defination
-    PerformFramework proc_perform;
-    proc_perform.code_ = "callfunc (print) (\"hello world!\")";  // parsed code.
-    proc_perform.parseCode();
-    proc_perform.code_ = proc_perform.getParsedCode();
+    PerformFramework* proc_perform;
+    proc_perform->code_ = "callfunc (print) (\"hello world!\")";  // parsed code.
+    proc_perform->parseCode();
+    proc_perform->code_ = proc_perform->getParsedCode();
 
     // process defination
-    Process stream_proc = {NULL, "", "", "", &proc_perform};
+    Process stream_proc = {NULL, "", "", "", proc_perform};
 
     // stream defination
-    Stream strm = {"", &stream_proc};
+    Stream strm = {"main", &stream_proc};
     return 1;
 }
