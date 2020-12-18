c74f340ff80897a4b5d664afc242054914874f10
RedoC-github <coderlovemedia@gmail.com>
2020-12-18 13:23:11 +0900

    core: stream upgrade

--- src/stream/stream.h
+++ src/stream/stream.h
@@ -26,11 +26,28 @@ enum StreamType {
     IFO,  // ifo(in, filter, out) stream. 3 process.
 };
 
-// stream structure
-struct Stream {
-    StreamType strm_type_;  // stream type
-    Process* proc_node_;  // process node. Never NULL
-    string strm_name_;  // stream name. just option
+// stream class
+class Stream {
+    public:
+        Stream* strm_node_next_;  // stream node -> next.
+        StreamType strm_type_;  // stream type
+        Process* proc_node_;  // process node. Never NULL
+        string strm_name_;  // stream name. just option
+
+        void ConnectProc(Process* proc_n) {
+            proc_node_->proc_next = proc_n;
+        }
+};
+
+class StreamCode {
+    public:
+        Stream* strm_node_;  // stream node
+        // :TODO module importer
+        string file_name_;  // file name
+
+        void ConnectStrm(Stream* strm_n) {
+            strm_node_->strm_node_next_ = strm_n;
+        }
 };
 
 #endif
