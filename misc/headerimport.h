5b44df361853d0403fd864b22a922d3f7da29fb6
RedoC-github <coderlovemedia@gmail.com>
2020-12-26 23:04:15 +0900

    misc, test: add test file

---
+++ misc/headerimport.h
@@ -0,0 +1,18 @@
+/*
+ * headerimport.h
+ *
+ * StreamCode Stream Source Header import.
+ * author: RedoC
+ *
+ * 2020, DiggieBro. All right reserved.
+ * This file is under MIT license.
+ * check LICENSE to get license information.
+ * */
+
+#include "../src/builtin/io.h"
+#include "../src/io/cio.h"
+#include "../src/module/module.h"
+#include "../src/module/importer.h"
+#include "../src/stream/process.h"
+#include "../src/stream/stream.h"
+#include "../src/stream/type.h"
